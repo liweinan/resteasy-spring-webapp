f5e0a94a38151ef3c393322688104165ac00494f
Weinan Li <l.weinan@gmail.com>
2019-03-06 22:04:29 +0800

    save progress

---
+++ src/main/java/io/weli/spring/rest/FooResource.java
@@ -0,0 +1,12 @@
+package io.weli.spring.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/foo")
+public class FooResource {
+   @GET
+   public String foo() {
+      return "foo";
+   }
+}
