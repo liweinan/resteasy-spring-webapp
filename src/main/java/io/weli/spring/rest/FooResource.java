f40e6c3b9500d8bcacb87eea771d158f39ae8275
Weinan Li <l.weinan@gmail.com>
2019-03-07 13:09:00 +0800

    complete

--- src/main/java/io/weli/spring/rest/FooResource.java
+++ src/main/java/io/weli/spring/rest/FooResource.java
@@ -1,9 +1,12 @@
 package io.weli.spring.rest;
 
+import org.springframework.stereotype.Controller;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
 @Path("/foo")
+@Controller
 public class FooResource {
    @GET
    public String foo() {
